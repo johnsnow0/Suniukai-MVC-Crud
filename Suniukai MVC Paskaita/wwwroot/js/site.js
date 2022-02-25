ff23fddb196029a8b41c8bc3d927c052d946ba76
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2022-02-25 16:02:18 +0200

    update

--- Suniukai MVC Paskaita/wwwroot/js/site.js
+++ Suniukai MVC Paskaita/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function pridetiPrekeKrepseliui() {
+    alert("Pridedu preke")
+    let kiekis = localStorage.getItem("krepselis");
+    if (kiekis == null) kiekis = "0";
+    localStorage.setItem("krepselis", Number.parseInt(kiekis) + 1);
+    kiekPrekiuKrepselyje();
+}
+function kiekPrekiuKrepselyje() {
+    document.getElementById("kiekis").innerHTML = localStorage.getItem("krepselis");
+}
+kiekPrekiuKrepselyje();
